6ee9bec3b08838961076f659c427203354fff419
Joel Deaguero <Joel Deaguero>
2016-02-28 20:42:08 -0800

    Set up for dev on windows, push to Linux

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 #!/usr/bin/env node
-var express = require('express');
+var async = require('async');
 var fs = require('fs');
+var express = require('express');
 var https = require('https');
 
-var ports = process.env.NODE_ENV === 'production'
-  ? [80, 443]
-  : [3442, 3443];
+var timeStartup = new Date();
+var isProduction = process.env.NODE_ENV === 'production';
+console.log('deaguero-org %s isProduction=%d', timeStartup.toUTCString(), isProduction);
+
+var ports = isProduction ? [80, 443] : [3442, 3443];
+var tlsFiles = ['./tls/key.pem', './tls/cert.pem'];
+var wantSSL;
+async.each(tlsFiles, function(filePath, callback) {
+	fs.access(filePath, fs.R_OK, (err) => {
+		console.log('deaguero-org no access to %s', filePath);
+		callback(err);
+	});
+}, function(err) {
+	if(isProduction && err) {
+		return;
+	}
+	wantSSL = !err;
+});
 
 var app = express();
+var server;
 
-var server = https.createServer(
-  {
-    key: fs.readFileSync('./tls/key.pem'),
-    cert: fs.readFileSync('./tls/cert.pem')
-  },
-  app
-);
+if(wantSSL) {
+  server = https.createServer(
+    {
+      key: fs.readFileSync(tlsFiles[0]),
+      cert: fs.readFileSync(tlsFiles[1])
+    },
+    app
+  );
 
-server.listen(ports[1], function () {
-  console.log('deaguero-org listening for HTTPS on port %d', ports[1]);
-});
+  server.listen(ports[1], function () {
+    console.log('deaguero-org listening for HTTPS on port %d', ports[1]);
+  });
+}
 
 app.listen(ports[0], function () {
   console.log('deaguero-org listening for HTTP on port %d', ports[0]);
--- linux/clean.sh
+++ linux/clean.sh
@@ -2,5 +2,3 @@
 systemctl stop deaguero-org
 git clean -f -d
 git pull origin master
-
-
