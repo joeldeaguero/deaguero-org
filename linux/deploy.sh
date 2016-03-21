bad2cd32786cffbf7006a6e3b93e0bbc5dc8396f
Joel Deaguero <joel.deaguero@gmail.com>
2016-03-21 00:27:59 +0000

    Update shrinkwrap and linux deploy steps for google OAuth2.0 login

--- linux/deploy.sh
+++ linux/deploy.sh
@@ -3,6 +3,9 @@ npm install
 mkdir tls
 cp /etc/letsencrypt/live/www.deaguero.org/cert.pem tls/cert.pem
 cp /etc/letsencrypt/live/www.deaguero.org/privkey.pem tls/key.pem
+mkdir oauth
+cp /etc/googledevelopers/live/www.deaguero.org/clientId.txt oauth/clientId.txt
+cp /etc/googledevelopers/live/www.deaguero.org/clientSecret.txt oauth/clientSecret.txt
 chown -R nobody:nobody .
 systemctl start deaguero-org
 
